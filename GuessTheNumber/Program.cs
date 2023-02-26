334465ebd6cdd7f8d5f369f6f4e7b2c30fd6f26c
LemaSed <lema_no@hotmail.no>
2023-02-26 21:30:56 +0100

    Testing github action

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -2,11 +2,12 @@
 
 namespace GuessTheNumber
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-		}
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is another test");
+        }
+    }
 }
